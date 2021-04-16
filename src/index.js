1b5ee409b046a65c9d71c59a1931396ed644a114
John Sheehy <sheehy.john@gmail.com>
2021-04-16 10:47:59 +0100

    refactor

--- src/index.js
+++ src/index.js
@@ -3,16 +3,8 @@ import config from './config';
 import BootScene from './scenes/Boot';
 import PreloaderScene from './scenes/Preloader';
 import MenuScene from './scenes/Menu';
-import GameScene from './scenes/Game';
-import ScoreHUD from './scenes/scoreHUD';
-
-//const game = new Phaser.Game(Object.assign(config, {
-new Phaser.Game(Object.assign(config, {
-  //scene: [BootScene, PreloaderScene, MenuScene, GameScene, ScoreHUD],
-  scene: [BootScene, PreloaderScene, MenuScene, GameScene],
-}));
-
-
-// this is commit just to get it to build again on AWS
-// showing to many levels and doesn't have skip button
-// these both are recent additions
+import GameScene from './scenes/Game'; 
+ 
+new Phaser.Game(Object.assign(config, { 
+    scene: [BootScene, PreloaderScene, MenuScene, GameScene],
+})); 
