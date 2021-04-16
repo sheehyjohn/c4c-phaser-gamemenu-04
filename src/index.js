89301469d455a9a691356b061b30d264339775b4
John Sheehy <sheehy.john@gmail.com>
2021-04-16 09:54:22 +0100

    comment for AWS build

--- src/index.js
+++ src/index.js
@@ -11,3 +11,8 @@ new Phaser.Game(Object.assign(config, {
   //scene: [BootScene, PreloaderScene, MenuScene, GameScene, ScoreHUD],
   scene: [BootScene, PreloaderScene, MenuScene, GameScene],
 }));
+
+
+// this is commit just to get it to build again on AWS
+// showing to many levels and doesn't have skip button
+// these both are recent additions
