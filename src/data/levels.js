f7fa617b0a2a4dfd93c037a2194e109b27fb12ea
John Sheehy <sheehy.john@gmail.com>
2021-04-19 11:55:31 +0100

    layer tidy up

--- src/data/levels.js
+++ src/data/levels.js
@@ -1,28 +1,27 @@
 // List of all levels with associated data. Could add all sorts of data points in here. The map is just an example.
 export default [
-  { name: 'Level 1', map: 'level-1' }, 
-  { name: 'Level 2', map: 'level-2' },
-  
-  { name: 'Level 3', map: 'level-3' },
-  { name: 'Level 4', map: 'level-4' },
-  
-  { name: 'Level 5', map: 'level-5' },
-  
-  { name: 'Level 6', map: 'level-6' },
-  
-  { name: 'Level 7', map: 'level-7' },
-  { name: 'Level 8', map: 'level-8' },
-  { name: 'Level 9', map: 'level-9' },
-  { name: 'Level 10', map: 'level-10' },
-  { name: 'Level 11', map: 'level-11' },
-  { name: 'Level 12', map: 'level-12' },
-  { name: 'Level 13', map: 'level-13' },
-  { name: 'Level 14', map: 'level-14' }, 
-  { name: 'Level 15', map: 'level-15' },
-  { name: 'Level 16', map: 'level-16' },
-  { name: 'Level 17', map: 'level-17' },
-  { name: 'Level 18', map: 'level-18' }//,,,
-/*  */
-  //{ name: 'Level 19', map: 'level-19' },
-  //{ name: 'Level 20', map: 'level-20' }
+    { name: 'Level 1', map: 'level-1' }, 
+    { name: 'Level 2', map: 'level-2' }, 
+    { name: 'Level 3', map: 'level-3' },
+    { name: 'Level 4', map: 'level-4' }, 
+    { name: 'Level 5', map: 'level-5' }, 
+
+    { name: 'Level 6', map: 'level-6' }, 
+    { name: 'Level 7', map: 'level-7' },
+    { name: 'Level 8', map: 'level-8' },
+    { name: 'Level 9', map: 'level-9' },
+    { name: 'Level 10', map: 'level-10' },
+
+    { name: 'Level 11', map: 'level-11' },
+    { name: 'Level 12', map: 'level-12' },
+    { name: 'Level 13', map: 'level-13' },
+    { name: 'Level 14', map: 'level-14' }, 
+    { name: 'Level 15', map: 'level-15' },
+    
+    { name: 'Level 16', map: 'level-16' },
+    { name: 'Level 17', map: 'level-17' },
+    { name: 'Level 18', map: 'level-18' },
+    { name: 'Level 19', map: 'level-19' },
+    { name: 'Level 20', map: 'level-20' }
+
 ];
